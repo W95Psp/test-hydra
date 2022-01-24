c0db299c46f6ddf04314eb46cca343c13a72f534
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-01-24 13:48:45 +0100

    test

--- flake.nix
+++ flake.nix
@@ -16,10 +16,11 @@
         defaultPackage.x86_64-linux = pkgs.writeText "x" "o";
         hydraJobs.x86_64-linux.test = pkgs.runCommand "test" {} ''
           mkdir -p $out/nix-support $out/test
-          echo 'doc Blurpon123 $out/test' > $out/nix-support/hydra-build-products
-          echo 'Xhere1643028260' > $out/test/a
-          echo 'Yhere1643028260' > $out/test/b
-          echo 'Zhere1643028260' > $out/test/c
+          # echo 'doc Blurpon123 $out/test' > $out/nix-support/hydra-build-products
+          echo "doc manual $out/test" >> $out/nix-support/hydra-build-products
+          echo 'Xhere1643028525' > $out/test/a
+          echo 'Yhere1643028525' > $out/test/b
+          echo '<b>Z/<b>here1643028525' > $out/test/index.html
         '';
       };
   
