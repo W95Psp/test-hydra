2e80470913a1fd45c561d3b306db313044614a20
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-01-24 14:35:09 +0100

    test

--- flake.nix
+++ flake.nix
@@ -18,9 +18,9 @@
           mkdir -p $out/nix-support $out/test
           echo 'doc Blurpon123 $out/test/a' >> $out/nix-support/hydra-build-products
           echo "doc manual $out/test" >> $out/nix-support/hydra-build-products
-          echo 'Xhere1643029267' > $out/test/a
-          echo 'Yhere1643029267' > $out/test/b
-          echo '<b>Z/<b>here1643029267' > $out/test/index.html
+          echo 'Xhere1643031309' > $out/test/a
+          echo 'Yhere1643031309' > $out/test/b
+          echo '<b>Z/<b>here1643031309' > $out/test/index.html
         '';
       };
   
