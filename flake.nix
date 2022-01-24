953aa0dd7f13f90a1fbc18c7aef90dd4861b6861
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-01-24 09:52:53 +0100

    test

---
+++ flake.nix
@@ -0,0 +1,21 @@
+
+{
+  description = "...";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, ...}:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+      rec {
+        packages.x86_64-linux.hey = pkgs.hello;
+        defaultPackage.x86_64-linux = pkgs.hello;
+      };
+}
+
+
+
