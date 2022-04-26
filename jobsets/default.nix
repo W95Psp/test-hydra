5356824096505ce0ea13f2c45e46188287623f5c
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-04-26 15:30:37 +0200

    snap

--- jobsets/default.nix
+++ jobsets/default.nix
@@ -1,5 +1,25 @@
-let pkgs = import <nixpkgs> {}; in {
-      jobsets = {
-        
+{
+  src
+}:
+let
+  pkgs = import <nixpkgs> {};
+  jobsetsAttrs = {
+    nixpkgs = {
+      enabled = 1;
+      hidden = false;
+      description = "TEST";
+      checkinterval = 0;
+      schedulingshares = 100;
+      enableemail = false;
+      enable_dynamic_run_command = false;
+      emailoverride = "";
+      keepnr = 3;
+      flake = "git+ssh://git@github.com/W95Psp/test-hydra";
+    }
+
+  };
+in {
+  jobsets = {
+    jobsets = pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs);
       };
 }
