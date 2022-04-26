18f700008b49111da5f26f1991f0118bf42265b0
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-04-26 12:50:13 +0200

    snap

--- flake.nix
+++ flake.nix
@@ -16,6 +16,9 @@
         defaultPackage.x86_64-linux = pkgs.writeText "x" "o";
         hydraJobs.x86_64-linux.test = pkgs.runCommand "test" {} ''
           mkdir -p $out/nix-support $out/test
+          echo "je suxxis là"
+          echo "je suis là"
+          echo "asdasjd"
           echo 'doc Blurpon123 $out/test/a' >> $out/nix-support/hydra-build-products
           echo "doc manual $out/test" >> $out/nix-support/hydra-build-products
           echo 'XXOHOHXhere1643031309XXX' > $out/test/a
