6ef5bd00db9c5d80d85b640fca9563c445cd9c2c
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-05-02 23:36:56 +0200

    snap

--- flake.nix
+++ flake.nix
@@ -19,11 +19,11 @@
           echo "je suxxis là"
           echo "je suis là"
           echo "asdasjd"
-          echo 'doc Blurpon123 $out/test/a' >> $out/nix-support/hydra-build-products
+          echo 'doc someting $out/test/a' >> $out/nix-support/hydra-build-products
           echo "doc manual $out/test" >> $out/nix-support/hydra-build-products
-          echo 'asdasd' > $out/test/a
-          echo 'XXOHOHYhere1643031309YYYaa' > $out/test/b
-          echo '<b>XXZOHOH/hey<b>here1643031309PPP' > $out/test/index.html
+          echo 'asdasdxxx' > $out/test/a
+          echo 'xxxx' > $out/test/b
+          echo '<b>xxxx/hey<b>here1643031309PPP' > $out/test/index.html
         '';
       };
   
