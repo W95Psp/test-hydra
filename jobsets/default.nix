a7b0523836952bb5473bd26c64abe09e78dd5bf2
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-05-02 15:43:50 +0200

    snap

--- jobsets/default.nix
+++ jobsets/default.nix
@@ -1,29 +1,32 @@
-{
-  nixpkgs, prs, declInput
-}:
-let
-  pkgs = import nixpkgs {};
-  jobsetsAttrs = {
-    nixpkgs = {
-      enabled = 1;
-      hidden = false;
-      description = "TEST";
-      checkinterval = 0;
-      schedulingshares = 100;
-      enableemail = false;
-      enable_dynamic_run_command = false;
-      emailoverride = "";
-      keepnr = 3;
-      flake = "git+ssh://git@github.com/W95Psp/test-hydra";
-    };
-  };
-in {
-  jobsets = builtins.throw "xxx";
-  # jobsets = pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs);
-  # jobsets = pkgs.runCommand "spec.json" {} ''
-  #   cat <<EOF
-  #   ${builtins.toJSON {inherit jobsetsAttrs prs;}}
-  #   EOF
-  #   cp ${pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs)} $out
-  # '';
-}
+_: ./test.json
+
+# {
+#   nixpkgs#@, prs, declInput
+#     ,...
+# }:
+# let
+#   pkgs = import nixpkgs {};
+#   jobsetsAttrs = {
+#     nixpkgs = {
+#       enabled = 1;
+#       hidden = false;
+#       description = "TEST";
+#       checkinterval = 0;
+#       schedulingshares = 100;
+#       enableemail = false;
+#       enable_dynamic_run_command = false;
+#       emailoverride = "";
+#       keepnr = 3;
+#       flake = "git+ssh://git@github.com/W95Psp/test-hydra";
+#     };
+#   };
+# in {
+#   jobsets = builtins.throw "xxx";
+#   # jobsets = pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs);
+#   # jobsets = pkgs.runCommand "spec.json" {} ''
+#   #   cat <<EOF
+#   #   ${builtins.toJSON {inherit jobsetsAttrs prs;}}
+#   #   EOF
+#   #   cp ${pkgs.writeText "spec.json" (builtins.toJSON jobsetsAttrs)} $out
+#   # '';
+# }
