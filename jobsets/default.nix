7c058c01d35a7959e9fa37600ed0b3810b6686c2
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-05-02 23:18:11 +0200

    snap

--- jobsets/default.nix
+++ jobsets/default.nix
@@ -11,20 +11,20 @@ let
     '') ];
     contents = builtins.toJSON contents;
   };
-  mk = id: info: {
+  mkPR = id: info: {
     name = "pr${id}";
-    value = {
-      enabled = 1;
-      type = 1;
-      hidden = false;
-      description = "PR ${id}: ${info.title}";
-      flake = "git+ssh://git@github.com/${info.head.repo.full_name}?ref=${info.head.ref}";
-      checkinterval = 300;
-      schedulingshares = 10;
-      enableemail = false;
-      emailoverride = "";
-      keepnr = 50;
-    };
+    value = mk "PR ${id}: ${info.title}" "git+ssh://git@github.com/${info.head.repo.full_name}?ref=${info.head.ref}";
+  };
+  mk = description: flake: {
+    inherit description flake;
+    enabled = 1;
+    type = 1;
+    hidden = false;
+    checkinterval = 300;
+    schedulingshares = 10;
+    enableemail = false;
+    emailoverride = "";
+    keepnr = 50;
   };
   attrsToList = l:
     builtins.attrValues (
@@ -35,5 +35,9 @@ let
 in
 {
   # jobsets = throw (builtins.toJSON prs);
-  jobsets = makeSpec (builtins.listToAttrs (map ({name, value}: mk name value) (attrsToList prs-value)));
+  jobsets = makeSpec (
+    builtins.listToAttrs (map ({name, value}: mkPR name value) (attrsToList prs-value)) // {
+      master = mk "master" "git+ssh://git@github.com/W95Psp/test-hydra";
+    }
+  );
 }
