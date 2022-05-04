21a5349da9919754a95b36bb8c21f5231e315bc5
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-05-04 15:10:14 +0200

    Update mer. 04 mai 2022 15:10:14 CEST

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
               sleep 1
           done
           echo "doc manual $out/test" >> $out/nix-support/hydra-build-products
-          echo '<b>Example HTML manual product<b>Date: mer. 04 mai 2022 15:08:06 CEST' > $out/test/index.html
+          echo '<b>Example HTML manual product<b>Date: mer. 04 mai 2022 15:10:14 CEST' > $out/test/index.html
           cat $ {test}/README.md >> $out/test/index.html
         '';
       };
